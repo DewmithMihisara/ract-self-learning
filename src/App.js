64c9e42c5ef348ad6d4f2037c4a2774aa7dde646
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-07 16:34:14 +0530

    rest

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App =()=> {
   return (
     <div className="App">
-      
+        <SampleComponent />
     </div>
   );
+}
+
+const SampleComponent =() => {
+  return (
+    <div>
+      <h1>Sample Component</h1>
+    </div>
+  )
 }
 
 export default App;
